ce47d82ac3b49aa6a45ff4eb505b08456f5f939a
Daniel <danielwetaas@gmail.com>
2024-12-01 14:33:08 +0100

    Fixed mistake, forgot part of puzzle text

--- Day 1/AoC2024D1P1.py
+++ Day 1/AoC2024D1P1.py
@@ -1,16 +1,26 @@
 data = []
 output = 0
 
-with open("Input.txt","r") as f:
+with open("Day 1\Input.txt","r") as f:
     list = f.readlines()
     splitted = [line.split() for line in list]
+
+    unsorted = []
     for line in splitted:
         temp = [int(line[0]),int(line[1])]
-        data.append(temp)
-    print(data)
-
-    for line in data:
-        output += abs(line[0]-line[1])
+        unsorted.append(temp)
+    print(unsorted)
+    
+    leftlist = []
+    rightlist = []
+    for digits in unsorted:
+        leftlist.append(digits[0])
+        rightlist.append(digits[1])
+    leftlist.sort()
+    rightlist.sort()
+    
+    for i in range(len(leftlist)):
+        output += abs(leftlist[i]-rightlist[i])
 
 print(output)
 
